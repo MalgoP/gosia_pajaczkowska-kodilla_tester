df71807b330edd36a193a9df0044443c1b501c02
malgorzata.holub <malgorzata>
2022-03-17 18:48:00 +0100

    Zadanie: Ćwiczymy selektory CSS

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -4,22 +4,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "c:/selenium-drivers/chrome/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 15);
         driver.get("https://allegro.pl/");
 
         WebElement button = driver.findElement(By.cssSelector("button[data-role='accept-consent']"));
         button.click();
-        WebElement labelCombo = driver.findElement(By.xpath("//select[1]"));
-        Select categorySelect = new Select(labelCombo);
-        categorySelect.selectByIndex(3);
-        WebElement inputField = driver.findElement(By.cssSelector("input[placeholder='czego szukasz?']"));
-        inputField.sendKeys("Mavic mini");
-        WebElement buttonSearch = driver.findElement(By.cssSelector("button[data-role='search-button']"));
-        buttonSearch.click();
+        WebElement inputField = driver.findElement(By.tagName("input"));
+        inputField.sendKeys("szczoteczka soniczna");
+        inputField.submit();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("section>article")));
+        List<WebElement> articleList = driver.findElements(By.cssSelector("section>article"));
+        for (WebElement webElement : articleList) {
+            System.out.println(webElement.getText());
+        }
     }
 }
